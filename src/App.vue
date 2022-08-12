393fcb86684e02a3c2fe7bea49895a59e1cf0bec
Jaron Pate <jaronpate@gmail.com>
2022-08-12 14:50:34 -0500

    Gonna do this differently

--- src/App.vue
+++ src/App.vue
@@ -1,341 +1,294 @@
-<template>
-  <div>
-    
-    <div id="app" style="margin-top: 15px">
-
-      <div class="profile">
-        <span class="profileIcon">
-          <img v-bind:src="summoner.icon" style="max-height: 64px; border-radius: 50%;">
-          <span id="level">{{ summoner.summonerLevel }}</span>
-        </span>
-        <h1>{{ summoner.displayName }}</h1>
-      </div>
-
-      <!-- <img v-bind:src="champion.icon" style="max-height: 64px;">
-      <h1>{{ champion.name }}</h1> -->
-      
-      <div class="perk-select" style="margin-bottom: 15px;">
-        Rune Page: <select v-model="selectedPageName" id="perkpage">
-          <option selected disabled hidden>Select a rune page...</option>
-          <option v-for="page in perkPages" v-bind:key="page.id">{{ page.name }}</option>
-        </select>
-        <button @click="savePage">Save Page</button>
-
-        <div class="styles">
-          <p>Primary Rune: {{ selectedPrimary.name }}</p>
-          <ul>
-            <li v-for="perk in primaryPerks" v-bind:key="perk.id">
-              {{ perk.name }}
-            </li>
-          </ul>
-          <p>Secondary Rune: {{ selectedSecondary.name }}</p>
-          <ul>
-            <li v-for="perk in secondaryPerks" v-bind:key="perk.id">
-              {{ perk.name }}
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <hr>
-
-      <div class="stored-perk-select">
-        Stored Pages: <select v-model="selectedStoredPageName" id="storedperkpage">
-          <option selected disabled hidden>Select a rune page...</option>
-          <option v-for="page in storedPages" v-bind:key="page.name">{{ page.name }}</option>
-        </select>
-        <button @click="loadPage">Load Page</button>
-        <button @click="removePage">Delete Page</button>
-      </div>
-
-    </div>
-
-    <footer>
-      <span style="margin-right: 10px">LCU - {{version}} | {{appName}} - {{appVer}}</span>
-    </footer>
-  </div>
-</template>
-
-<script>
-import riot from './lib/riotApi.js'
-const invoke = window.__TAURI__.invoke;
-const app = window.__TAURI__.app;
-
-let ddVer;
-
-// Window titlebar functionality
-import { appWindow } from '@tauri-apps/api/window'
-
-document
-  .getElementById('minimize')
-  .addEventListener('click', () => appWindow.minimize())
-document
-  .getElementById('maximize')
-  .addEventListener('click', () => appWindow.toggleMaximize())
-document
-  .getElementById('close')
-  .addEventListener('click', () => appWindow.close())
-
-export default {
-  name: 'App',
-  data: function() {
-    return {
-      selectedPageName: "Select a rune page...",
-      selectedStoredPageName: "Select a rune page...",
-      storedPages: [],
-      perkPages: {},
-      selectedPage: {},
-      selectedPerks: [],
-      primaryPerks: [],
-      secondaryPerks: [],
-      selectedPrimary: {},
-      selectedSecondary: {},
-      perks: {},
-      styles: {},
-      connData: {}
-    }
-  },
-  created(){
-    let storedPages = window.localStorage.getItem("pages")
-    if (storedPages) {
-      this.storedPages = JSON.parse(storedPages);
-    }
-  },
-  asyncComputed: {
-    async summoner() {
-      let connData = await invoke('get_credentials')
-        .then(data => data).catch(e => {
-          console.log(e)
-          invoke("error");
-        });
-      console.log(connData);
-      this.connData = connData
-      let sum =  await invoke('lcu', {
-        endpoint: '/lol-summoner/v1/current-summoner',
-        port: connData.port,
-        password: connData.password
-      }).then((data) => data).catch(e => console.log(e));
-
-      // Get other LCU data
-      let perks = await invoke('lcu', {
-        endpoint: `/lol-perks/v1/perks`,
-        port: connData.port,
-        password: connData.password
-      }).then(data => data);
-
-      let styles = await invoke('lcu', {
-        endpoint: `/lol-perks/v1/styles`,
-        port: connData.port,
-        password: connData.password
-      }).then(data => data);
-
-      let sumPerks = await invoke('lcu', {
-        endpoint: `/lol-perks/v1/pages`,
-        port: connData.port,
-        password: connData.password
-      }).then(data => data);
-      sumPerks = sumPerks.filter(page => page.isDeletable === true);
-
-      this.perks = perks;
-      this.styles = styles;
-      this.perkPages = sumPerks;
-      console.log(sumPerks)
-
-      sum.icon = `http://ddragon.leagueoflegends.com/cdn/${ddVer}/img/profileicon/${sum.profileIconId}.png`
-      return sum;
-    },
-    async version() {
-      return await riot("get", "/realms/na.json")
-        .then(data => data.v)
-    },
-    async appName() {
-      return await app.getName()
-        .then(data => data)
-    },
-    async appVer() {
-      return await app.getVersion()
-        .then(data => data)
-    },
-    champions: {
-      async get() {
-        return await riot("get", `/cdn/${ddVer}/data/en_US/champion.json`)
-          .then((res) => res.data)
-      },
-      lazy: true
-    }
-  },
-  methods:{
-    savePage(){
-      window.localStorage.setItem(this.selectedPage.name, this.selectedPage)
-      let pages = JSON.parse(window.localStorage.getItem("pages"))
-      if (pages !== null) {
-        pages.push(this.selectedPage)
-        window.localStorage.setItem("pages", JSON.stringify(pages))
-      } else {
-        window.localStorage.setItem("pages", JSON.stringify([this.selectedPage]))
-      }
-      this.storedPages = pages;
-    },
-    getPages(){
-      let pages = JSON.parse(window.localStorage.getItem("pages"))
-      return pages
-    },
-    loadPage(){
-      const self = this;
-      let data = this.storedPages.find(test => test.name === this.selectedStoredPageName);
-      invoke('post', {
-        endpoint: `/lol-perks/v1/pages/${data.id}`,
-        port: self.connData.port,
-        password: self.connData.password,
-        data: data
-      }).then((data) => data).catch(e => console.log(e));
-    },
-    removePage(){
-      let pages = JSON.parse(window.localStorage.getItem("pages"));
-      let i = pages.findIndex(test => test.name === this.selectedStoredPageName);
-      console.log(i)
-      console.log(this.selectedStoredPageName)
-      console.log(pages)
-      if(i !== -1){
-        pages.splice(i, 1);
-        window.localStorage.setItem("pages", JSON.stringify(pages));
-        this.storedPages = pages;
-      }
-    }
-  },
-  watch: {
-    summoner(data) {
-      console.log(data)
-    },
-    selectedPageName(data) {
-      // Setup Perk Picker upon page select
-      const self = this;
-      this.selectedPage = this.perkPages.find(page => page.name === data)
-      console.log(this.selectedPage)
-      this.selectedPerks = [];
-      this.selectedPage.selectedPerkIds.forEach(function(perk){
-        let foundPerk = self.perks.find(test => test.id === perk)
-        foundPerk.icon = `https://${self.connData.address}:${self.connData.port}${foundPerk.iconPath}`
-        self.selectedPerks.push(foundPerk)
-      })
-      this.primaryPerks = this.selectedPerks.slice(0,4)
-      this.secondaryPerks = this.selectedPerks.slice(4,9)
-      // console.log(this.primaryPerks, this.secondaryPerks)
-      this.selectedPrimary = this.styles.find(test => test.id === this.selectedPage.primaryStyleId)
-      this.selectedSecondary = this.styles.find(test => test.id === this.selectedPage.subStyleId)
-      console.log(this.perkPages)
-    },
-    version(vv){
-      console.log(vv)
-      ddVer = vv
-      this.$asyncComputed.champions.update();
-      riot("get", `/cdn/${vv}/data/en_US/champion.json`).then(function (res) {
-        let champs = Object.keys(res.data);
-        let champ = champs[Math.floor(Math.random() * ((champs.length - 1) - 0) + 0)]
-        document.body.style.background = `linear-gradient(0deg, rgba(45, 45, 45, 0.94), rgba(0, 0, 0, 0.94)), 
-        url(${`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${res.data[champ].id}_0.jpg`})`
-      });
-    }
-  }
-}
+<script setup lang="ts">
+import settings from "iconoir/icons/settings.svg";
+import info from "iconoir/icons/info-empty.svg";
+import search from "iconoir/icons/search.svg";
 </script>
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&display=swap');
-html{
-  background-size:cover;
-  height: 100%;
-  overflow: hidden;
-}
-body{
-  font-family: 'Source Sans Pro', sans-serif;
-  margin: auto;
-  padding: 2rem;
-  color: white;
-  background-position: center !important;
-  background-size: cover !important;
+<template>
+    <header>
+        <div class="wordmark">
+            <div class="logo"><img src="./assets/logo.jpg" /></div>
+            <!-- <h3>Rift Buddy</h3> -->
+        </div>
+        <div class="search">
+            <input type="text" :class="{'not-loading' : (!loading && results.length < 1) }" v-model="query" @keyup="find" placeholder="Search for a champion" />
+            <div class="results" v-if="results.length && !loading">
+                <template v-if="loading">
+                    <div class="loading"></div>
+                </template>
+                <div class="result" v-else-if="results.length" v-for="champ in results" @click="select(champ)">
+                    <img :src="champ_img(champ.image.full)"/>
+                    <div class="info">
+                        <h4>{{champ.name}}</h4>
+                        <p><em>{{champ.title}}</em></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="options">
+            <img class="icon" :src="settings" />
+        </div>
+    </header>
+    <div class="main">
+
+        <div class="champ" v-if="champ">
+            <img class="profile" :src="champ_img(champ.image.full)" />
+            <div class="info">
+                <h2>{{champ.name}}</h2>
+                <p><em>{{champ.tags.join(', ')}}</em></p>
+            </div>
+            <div class="ff"></div>
+            <div class="info flex-left">
+                <img class="spell" :title="`${champ.passive.name}\n${champ.passive.description}`" :src="passive_img(champ.passive.image.full)" />
+                <img class="spell" :title="`${spell.name}\n${spell.description}`" v-for="spell in champ.spells" :src="spell_img(spell.image.full)" />
+            </div>
+        </div>
+        <template v-else-if="loading">
+            <div class="loading"></div>
+        </template>
+    </div>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+
+type Champ = Record<string, any>;
+
+export default {
+    data() {
+        return {
+            query: "",
+            loading: false,
+            api_version: null,
+            results: [],
+            champs: [],
+            champ: null
+        };
+    },
+    methods: {
+        champ_img(champ_img_full: string) {
+            return `http://ddragon.leagueoflegends.com/cdn/${this.api_version}/img/champion/${champ_img_full}`;
+        },
+        passive_img(passive_img_full: string) {
+            return `http://ddragon.leagueoflegends.com/cdn/${this.api_version}/img/passive/${passive_img_full}`;
+        },
+        spell_img(spell_img_full: string) {
+            return `http://ddragon.leagueoflegends.com/cdn/${this.api_version}/img/spell/${spell_img_full}`;
+        },
+        find(){
+            if(this.query.trim().length < 1) { return this.results = [] };
+            this.results = this.champs.filter((champ: Champ) => champ.name.toLowerCase().includes(this.query.toLowerCase())).slice(0, 10);
+        },
+        select(champ: Champ){
+            this.loading = true;
+            this.query = '';
+            this.results = [];
+            axios.get(`https://ddragon.leagueoflegends.com/cdn/${this.api_version}/data/en_US/champion/${champ.id}.json`).then((res) => {
+                this.champ = Object.values(res.data.data)[0];
+                console.log(this.champ)
+            }).finally(() => {
+                this.loading = false;
+            });
+        }
+    },
+    mounted() {
+        axios.get("https://ddragon.leagueoflegends.com/api/versions.json").then((res) => {
+            this.api_version = res.data[0];
+        }).then(() => {
+            axios.get(`https://ddragon.leagueoflegends.com/cdn/${this.api_version}/data/en_US/champion.json`).then((res) => {
+                this.champs = Object.values(res.data.data);
+            });
+        })        
+    },
+};
+</script>
+
+<style scoped lang="less">
+@font-face {
+    font-family: "Inter", sans-serif;
+    src: url("./assets/Inter.ttf") format("ttf");
 }
 
-.profile{
-  display: flex;
-  flex-direction: row;
-  justify-content: left;
-  align-items: center;
-  margin-bottom: 15px;
+.flex-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-.profile img{
-  border: 3px solid rgba(128, 128, 128, 0.35);
-  padding: 3px;
+.flex-left {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
 }
 
-.profile h1{
-  margin-left: -10px;
+.ff {
+    width: 100%;
 }
 
-#level{
-  font-size: 1em;
-  margin: 0;
-  position: relative;
-  background: #141414;
-  padding: 3px 6px;
-  border-radius: 100px;
-  position: relative;
-  left: -50%;
+.loading {
+    border: 5px solid fade(#222, 30%); /* Light grey */
+    border-top: 5px solid fade(#222, 60%);
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    animation: spin 2s linear infinite;
+    margin: auto;
 }
 
-nav{
-  background-color: #323233;
-  height: 30px;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 15px;
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
 }
 
-.nav-logo{
-  margin-left: 10px;
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+input {
+    font-family: "Inter", sans-serif;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    border: 0;
+    outline: 0;
+    padding: 10px 10px;
+    border-radius: 5px;
+    width: 100%;
+
+    &.not-loading {
+        border-bottom-left-radius: 5px !important;
+        border-bottom-right-radius: 5px !important;
+    }
 }
 
-.nav-btns{
-  -webkit-app-region: no-drag;
+.champ {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    .profile {
+        width: 10%;
+        box-shadow: #111 0px 0px 10px;
+    }
+    .info {
+        text-align: left;
+        margin-left: 20px;
+        h2 {
+            margin: 0;
+        }
+        em {
+            font-size: 0.8em;
+            color: #999;
+        }
+        p {
+            margin: 0;
+        }
+        .spell {
+            width: 40px;
+            margin: 0 5px;
+            border-radius: 5px;
+            border: #111 solid 2px;
+        }
+    }
 }
 
-.nav-btns i, .nav-btns img, .nav-btns svg{
-  padding: 9px 3px;
-  width: 26px;
-  height: 0.9em;
-  text-align: center;
+.search {
+    width: 100%;
+    margin: auto 15px;
+    position: relative;
+
+    .results {
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        background-color: #333;
+        border-radius: 5px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        width: 100%;
+        padding: 5px 0;
+        position: absolute;
+        text-align: left;
+
+        .result {
+            padding: 5px 10px;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            .info {
+                margin-left: 15px;
+            }
+            h4,
+            p {
+                line-height: 18px;
+                margin: 0;
+            }
+            h4 {
+                font-weight: 600;
+            }
+            em {
+                font-size: 0.8em;
+                color: #999;
+            }
+            img {
+                width: 40px;
+            }
+
+            &:last-of-type {
+                margin-bottom: 0;
+            }
+            &:hover {
+                background-color: fade(#222, 30%);
+                cursor: pointer;
+            }
+        }
+    }
+
+    input {
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+    }
 }
 
-.nav-btns i:hover, .nav-btns img:hover, .nav-btns svg:hover{
-  background-color: #414141;
-  cursor: pointer;
+.main {
+    margin: 10px 30px 0;
 }
 
-#close:hover{
-  /* background-color: rgb(201, 59, 59); */
-  background-color: #9e3b3b;
+.icon {
+    filter: invert(0.9);
+    margin-left: 6px;
+    transition: filter 0.1s linear;
+
+    &:hover {
+        filter: invert(0.7);
+        cursor: pointer;
+    }
 }
 
-footer{
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  /* background: #323233; */
-  color: rgba(255, 255, 255, 0.534);
-  font-size: 0.85em;
-  padding: 3px;
-  text-align: right;
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    // background-color: fade(#333, 50%);
+    // background: linear-gradient(90deg, fade(#333, 70%), fade(#111, 95%)), url('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg') no-repeat center center;
+    padding: 10px 15px;
 }
 
+.wordmark {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    h3 {
+        margin: 0 0 0 10px;
+        color: #b39556;
+    }
+}
+
+.logo {
+    img {
+        height: 30px;
+    }
+}
 </style>
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-let webpack = require("webpack");
-
-module.exports = {
-  chainWebpack: config => {
-    if (process.platform !== "darwin") {
-        config.plugin('IgnorePlugin').use(webpack.IgnorePlugin, [{ resourceRegExp: /^fsevents$/ }])
-    }
-  }
-}
