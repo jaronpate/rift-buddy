2ba2d1f046ade5596e974d1716a32c630af089b7
Jaron Pate <jaronpate@gmail.com>
2022-08-16 10:12:18 -0500

    UI starting to take shape and requests to league client working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,84 @@
   windows_subsystem = "windows"
 )]
 
+use league_client_connector::LeagueClientConnector;
+use league_client_connector::RiotLockFile;
+use tauri::{
+  Manager,
+};
+
+use reqwest::Client;
+use std::fs::File;
+use std::io::Read;
+
+struct AppState {
+  client: Client,
+  credentials: RiotLockFile,
+}
+
+#[tauri::command]
+fn close_splash(window: tauri::Window) {
+  println!("Closing splashscreen");
+  // Close splashscreen
+  if let Some(splash) = window.get_window("splash") {
+    splash.close().expect("Failed to close splashscreen");
+  }
+  // Show main window
+  if let Some(win) = window.get_window("main") {
+    win.show().expect("Failed to show main window");
+  }
+}
+
+#[tauri::command]
+fn get_credentials() -> Result<RiotLockFile, String> {
+  let lockfile = LeagueClientConnector::parse_lockfile();
+  match lockfile {
+    Ok(lockfile) => Ok(lockfile.into()),
+    Err(_) => Err("League Client not found.".into())
+  }
+}
+
+#[tauri::command]
+async fn lcu(state: tauri::State<'_, AppState>, endpoint: String) -> Result<serde_json::Value, String> {
+  println!("https://{}:{}{}", state.credentials.address, state.credentials.port, endpoint);
+  let body: serde_json::Value = state.client.get(format!("https://{}:{}{}", state.credentials.address, state.credentials.port, endpoint))
+  .basic_auth("riot", Some(state.credentials.password.clone()))
+  .send()
+  .await
+  .expect("Failed to send request")
+  .json::<serde_json::Value>()
+  .await
+  .expect("Failed to parse response");
+
+  Ok(body.into())
+}
+
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      let cert_path = app.path_resolver()
+        .resolve_resource("./riotgames.pem")
+        .expect("Unable to find cert");
+      Ok({
+        // Self signed LCU Cert
+        let mut buf = Vec::new();
+        File::open(cert_path).expect("Unable to open")
+          .read_to_end(&mut buf).unwrap();
+        let cert = reqwest::Certificate::from_pem(&buf).unwrap();
+        // Build reqwest client
+        let client = Client::builder()
+          .add_root_certificate(cert)
+          .build()
+          .unwrap();
+        let credentials = get_credentials().expect("League Client not found.");
+        app.manage(AppState {
+          client,
+          credentials
+        });
+      })
+
+    })
+    .invoke_handler(tauri::generate_handler![close_splash, get_credentials, lcu])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -30,8 +30,7 @@ import search from "iconoir/icons/search.svg";
         </div>
     </header>
     <div class="main">
-
-        <div class="champ" v-if="champ">
+        <div class="champ" v-if="champ.id">
             <img class="profile" :src="champ_img(champ.image.full)" />
             <div class="info">
                 <h2>{{champ.name}}</h2>
@@ -39,11 +38,47 @@ import search from "iconoir/icons/search.svg";
             </div>
             <div class="ff"></div>
             <div class="info flex-left">
-                <img class="spell" :title="`${champ.passive.name}\n${champ.passive.description}`" :src="passive_img(champ.passive.image.full)" />
-                <img class="spell" :title="`${spell.name}\n${spell.description}`" v-for="spell in champ.spells" :src="spell_img(spell.image.full)" />
+                <div class="spell">
+                    <h3>P</h3>
+                    <img :title="`${champ.passive.name}\n${champ.passive.description}`" :src="passive_img(champ.passive.image.full)" />
+                </div>
+                <div class="spell" v-for="(spell, i) in champ.spells">
+                    <h3>{{spells[i]}}</h3>
+                    <img :title="`${spell.name}\n${spell.description}`" :src="spell_img(spell.image.full)" />
+                </div>
             </div>
         </div>
-        <template v-else-if="loading">
+        <div class="runes" v-if="runes.length">
+            <div class="rune-page">
+                <div class="select-main-rune">
+                    <div class="main-rune" v-for="rune in runes" @click="select_main_rune(rune)">
+                        <img :title="rune.name" :src="rune_img(rune.icon)" :class="{'selected': page.main.path?.id === rune.id}"/>
+                    </div>
+                </div>
+                <div class="rune-set" v-if="page.main.path" v-for="(slot, i) in page.main.path.slots">
+                    <div class="rune" :class="{'keystones': i === 0}" v-for="entry in slot.runes">
+                        <img :title="entry.name" :src="rune_img(entry.icon)" />
+                    </div>
+                </div>
+            </div>
+
+            
+            <div class="rune-page">
+                <div class="select-main-rune">
+                    <template v-for="rune in runes">
+                        <div class="main-rune" v-if="rune.id !== page.main.path?.id" @click="page.secondary.path = rune" >
+                            <img :title="rune.name" :src="rune_img(rune.icon)" :class="{'selected': page.secondary.path?.id === rune.id}"/>
+                        </div>
+                    </template>
+                </div>
+                <div class="rune-set" v-if="page.secondary.path" v-for="(slot, i) in page.secondary.path.slots">
+                    <div class="rune" v-if="i !== 0" v-for="entry in slot.runes">
+                        <img :title="entry.name" :src="rune_img(entry.icon)" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <template v-if="loading">
             <div class="loading"></div>
         </template>
     </div>
@@ -51,18 +86,35 @@ import search from "iconoir/icons/search.svg";
 
 <script lang="ts">
 import axios from "axios";
+import { invoke } from '@tauri-apps/api/tauri';
 
 type Champ = Record<string, any>;
+type Rune = Record<string, any>;
+type ConnData = Record<string, any>;
 
 export default {
     data() {
         return {
+            spells: ["Q", "W", "E", "R"],
             query: "",
             loading: false,
             api_version: null,
-            results: [],
-            champs: [],
-            champ: null
+            conn_data: {} as ConnData,
+            results: [] as Champ[],
+            champs: [] as Champ[],
+            runes: [] as Rune[],
+            champ: {} as Champ,
+            page: {
+                name: "Test",
+                main: {
+                    path: {} as Rune | null,
+                    keystone: {} as Rune | null,
+                },
+                secondary: {
+                    path: {} as Rune | null,
+                    keystone: {} as Rune | null,
+                }
+            }
         };
     },
     methods: {
@@ -75,6 +127,9 @@ export default {
         spell_img(spell_img_full: string) {
             return `http://ddragon.leagueoflegends.com/cdn/${this.api_version}/img/spell/${spell_img_full}`;
         },
+        rune_img(rune_img_full: string) {
+            return `https://ddragon.canisback.com/img/${rune_img_full}`;
+        },
         find(){
             if(this.query.trim().length < 1) { return this.results = [] };
             this.results = this.champs.filter((champ: Champ) => champ.name.toLowerCase().includes(this.query.toLowerCase())).slice(0, 10);
@@ -84,19 +139,38 @@ export default {
             this.query = '';
             this.results = [];
             axios.get(`https://ddragon.leagueoflegends.com/cdn/${this.api_version}/data/en_US/champion/${champ.id}.json`).then((res) => {
-                this.champ = Object.values(res.data.data)[0];
+                this.champ = Object.values(res.data.data)[0] as Champ;
                 console.log(this.champ)
             }).finally(() => {
                 this.loading = false;
             });
+        },
+        select_main_rune(rune: any){
+            if(this.page.secondary.path?.id === rune.id){
+                this.page.secondary.path = null;
+            }
+            this.page.main.path = rune;
         }
     },
     mounted() {
+        invoke('get_credentials')
+            .then((data) => {
+                this.conn_data = data as ConnData;
+                console.log(this.conn_data)
+            }).catch(console.error);
+        invoke('lcu', {endpoint: '/lol-summoner/v1/current-summoner'})
+            .then((data) => {
+                console.log(data)
+            }).catch(console.error);
         axios.get("https://ddragon.leagueoflegends.com/api/versions.json").then((res) => {
             this.api_version = res.data[0];
         }).then(() => {
             axios.get(`https://ddragon.leagueoflegends.com/cdn/${this.api_version}/data/en_US/champion.json`).then((res) => {
                 this.champs = Object.values(res.data.data);
+            });
+            axios.get(`http://ddragon.leagueoflegends.com/cdn/${this.api_version}/data/en_US/runesReforged.json`).then((res) => {
+                this.runes = res.data;
+                console.log(this.runes)
             });
         })        
     },
@@ -123,6 +197,74 @@ export default {
 
 .ff {
     width: 100%;
+}
+
+.runes {
+    margin-top: 25px;
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-start;
+}
+
+.rune-page {
+    width: 35%;
+}
+
+.rune-set {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+.select-main-rune{
+    margin: 15px 0;
+    .main-rune{
+        display: inline;
+        margin: 0 5px;
+
+        img {
+            border: #111 solid 2px;
+            background-color: fade(#111, 50%);
+            padding: 6px;
+            width: 18px;
+            border-radius: 99px;
+            transition: outline linear 0.1s;
+            outline-color: #b39556;
+            outline: #d6414100 solid 2px;
+
+            &:hover, &.selected {
+                outline-color: #b39556;
+                opacity: 0.9;
+                cursor: pointer;
+            }
+        }
+    }
+}
+
+.rune {
+    display: inline;
+    margin: 0 5px;
+
+    &.keystones {
+        margin: 0 5px 15px 5px;
+    }
+
+    img {
+        border: #111 solid 2px;
+        background-color: fade(#111, 50%);
+        padding: 1px;
+        border-radius: 99px;
+        width: 45px;
+        transition: outline linear 0.1s;
+        outline-color: #b39556;
+        outline: #d6414100 solid 2px;
+
+        &:hover{
+            outline-color: #b39556;
+            opacity: 0.9;
+            cursor: pointer;
+        }
+    }
 }
 
 .loading {
@@ -176,6 +318,7 @@ input {
         margin-left: 20px;
         h2 {
             margin: 0;
+            font-size: 28px;
         }
         em {
             font-size: 0.8em;
@@ -185,10 +328,24 @@ input {
             margin: 0;
         }
         .spell {
-            width: 40px;
-            margin: 0 5px;
-            border-radius: 5px;
-            border: #111 solid 2px;
+            margin-top: -15px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+
+            h3 {
+                margin: 0;
+                font-size: 0.9em;
+                // color: #999;
+            }
+
+            img {
+                width: 40px;
+                margin: 0 5px;
+                border-radius: 5px;
+                border: #111 solid 2px;
+            }
         }
     }
 }
@@ -210,6 +367,7 @@ input {
         padding: 5px 0;
         position: absolute;
         text-align: left;
+        z-index: 10;
 
         .result {
             padding: 5px 10px;
@@ -273,6 +431,12 @@ header {
     // background-color: fade(#333, 50%);
     // background: linear-gradient(90deg, fade(#333, 70%), fade(#111, 95%)), url('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg') no-repeat center center;
     padding: 10px 15px;
+}
+
+.options {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 .wordmark {
